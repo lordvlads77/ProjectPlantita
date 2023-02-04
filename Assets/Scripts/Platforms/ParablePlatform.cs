20d4409b2e70307c8fcf6d2eb7f613f40a8f98fd
chiro <jorge.del1991@yahoo.com.mx>
2023-02-04 16:10:34 -0600

    Parabola platform

---
+++ Assets/Scripts/Platforms/ParablePlatform.cs
@@ -0,0 +1,70 @@
+using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Platforms
+{
+    public class ParablePlatform : MonoBehaviour, IMovementPlatform
+    {
+        [FormerlySerializedAs("ParableOffset")] 
+        public float parableOffset = 4;
+        public float height = -5;
+        public float speed = 1;
+        private bool _moveToRight = true;
+        private float _animation;
+        private Vector2 _initPosition;
+
+        private void Awake()
+        {
+            _initPosition = transform.position;
+        }
+
+        public void FixedUpdate()
+        {
+            Move();
+        }
+
+        public void Move()
+        {
+            _animation += Time.deltaTime * speed;
+            _animation %= 5f;
+
+            Vector2 position;
+
+            if (_moveToRight)
+            {
+                position = Parabola(
+                    _initPosition, 
+                    new(_initPosition.x + parableOffset, _initPosition.y), 
+                    height, 
+                    _animation / 5f);
+            }
+            else
+            {
+                position = Parabola(
+                    new(_initPosition.x + parableOffset, _initPosition.y), 
+                    _initPosition, 
+                    height, 
+                    _animation / 5f);
+            }
+
+            if (Math.Abs(position.y - _initPosition.y) < .4f)
+            {
+                _moveToRight = !_moveToRight;
+            }
+            else
+            {
+                transform.position = position;
+            }
+        }
+        
+        public static Vector2 Parabola(Vector2 start, Vector2 end, float height, float t)
+        {
+            Func<float, float> f = x => -4 * height * x * x + 4 * height * x;
+
+            var mid = Vector2.Lerp(start, end, t);
+
+            return new Vector2(mid.x, f(t) + Mathf.Lerp(start.y, end.y, t));
+        }
+    }
+}
