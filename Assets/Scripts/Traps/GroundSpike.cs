d530548fe695fd1ea6679c08688b3e464fccbdfc
chiro <jorge.del1991@yahoo.com.mx>
2023-02-04 18:29:34 -0600

    ground spike tramp'

--- Assets/Scripts/Traps/GroundSpike.cs
+++ Assets/Scripts/Traps/GroundSpike.cs
@@ -16,18 +16,34 @@ namespace Traps
         public float activeTime = 2;
 
         private BoxCollider2D _boxCollider;
-        
-        
+        private float _nextActive;
+        private float _nextHidden;
 
         private void Awake()
         {
             _boxCollider = gameObject.GetComponent<BoxCollider2D>();
             _boxCollider.enabled = false;
+            _nextActive = Time.time + hiddenTime;
+            _nextHidden = 0;
         }
 
-        private void OnCollisionEnter(Collision collision)
+        public void FixedUpdate()
         {
-            var life = collision.gameObject.GetComponent<CharacterLife>();
+            if (_nextActive < Time.time && !_boxCollider.enabled)
+            {
+                _boxCollider.enabled = true;
+                _nextHidden = Time.time + activeTime;
+            }
+            else if(_nextHidden < Time.time && _boxCollider.enabled)
+            {
+                _boxCollider.enabled = false;
+                _nextActive = Time.time + hiddenTime;
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            var life = other.GetComponent<CharacterLife>();
             
             if(life == null) return;
 
