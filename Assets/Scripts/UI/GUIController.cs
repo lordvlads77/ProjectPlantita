4007b65894bbb54f725c0ee408c81585ca7362c3
Eva Luna Álvarez <zap363@amerike.edu.mx>
2023-02-04 19:11:37 -0600

    UI | Level Selector | Prep and Configurations for Levels Done

--- Assets/Scripts/UI/GUIController.cs
+++ Assets/Scripts/UI/GUIController.cs
@@ -7,8 +7,28 @@ public class GUIController : MonoBehaviour
 {
     public static GUIController Instance { get; private set; }
 
+    [Header("Panels")] 
+    [SerializeField] private GameObject _mainMenuPanel = default;
+    [SerializeField] private GameObject _levelSelectPanel = default;
+    
     public void ButtonPlay()
     {
-        
+        _mainMenuPanel.SetActive(false);
+        _levelSelectPanel.SetActive(true);
+    }
+
+    public void FirstLevel()
+    {
+        // The Logic for the acces to the first level goes here.
+    }
+
+    public void SecondLevel()
+    {
+        // The Logic to access to the second level goes here.
+    }
+
+    public void ThirdLevel()
+    {
+        // The Logic to access to the third level goes here.
     }
 }
